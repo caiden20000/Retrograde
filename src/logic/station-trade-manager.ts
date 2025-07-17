aaecfdb4ee8b352d24d697fbec4e8a1c07cbd826
c2k <121005054+caiden20000@users.noreply.github.com>
2025-07-17 05:25:14 +0000

    Remove commented out code

--- src/logic/station-trade-manager.ts
+++ src/logic/station-trade-manager.ts
@@ -19,7 +19,11 @@ const RANDOM_TRADE_RANGE = 250;
 
 export function runTradeForSystem(system: System, revolutions: number): System {
   let newSystem = cloneSystem(system);
-  for (let currentRevolution = 0; currentRevolution <= revolutions; currentRevolution++) {
+  for (
+    let currentRevolution = 0;
+    currentRevolution <= revolutions;
+    currentRevolution++
+  ) {
     for (const station of newSystem) {
       const random = Math.random();
       if (random > TRADE_CHANCE) continue;
@@ -27,19 +31,51 @@ export function runTradeForSystem(system: System, revolutions: number): System {
       const shortages = getShortagesFromStation(station);
       if (shortages.length > 0) {
         const itemType = randomOf(shortages);
-        const itemQuantity = Trd.getItemBaseQuantity(station.tradeInventory, itemType) * SHORTAGE_THRESHOLD;
-        const supplier = getNearestSupplier(system, station, itemType, itemQuantity);
+        const itemQuantity =
+          Trd.getItemBaseQuantity(station.tradeInventory, itemType) *
+          SHORTAGE_THRESHOLD;
+        const supplier = getNearestSupplier(
+          system,
+          station,
+          itemType,
+          itemQuantity
+        );
         if (supplier == null) break;
-        const newStations = tradeBetween(station, supplier, itemType, itemQuantity);
-        newSystem = updateStationInSystem(newSystem, station, newStations.requester);
-        newSystem = updateStationInSystem(newSystem, supplier, newStations.supplier);
+        const newStations = tradeBetween(
+          station,
+          supplier,
+          itemType,
+          itemQuantity
+        );
+        newSystem = updateStationInSystem(
+          newSystem,
+          station,
+          newStations.requester
+        );
+        newSystem = updateStationInSystem(
+          newSystem,
+          supplier,
+          newStations.supplier
+        );
       } else {
         // If no shortage, random trade to random station.
-        const supplier = getRandomNeighborInRange(system, station, RANDOM_TRADE_RANGE);
+        const supplier = getRandomNeighborInRange(
+          system,
+          station,
+          RANDOM_TRADE_RANGE
+        );
         const newStations = randomTradeBetween(station, supplier);
         if (newStations != null) {
-          newSystem = updateStationInSystem(newSystem, station, newStations.requester);
-          newSystem = updateStationInSystem(newSystem, supplier, newStations.supplier);
+          newSystem = updateStationInSystem(
+            newSystem,
+            station,
+            newStations.requester
+          );
+          newSystem = updateStationInSystem(
+            newSystem,
+            supplier,
+            newStations.supplier
+          );
         }
       }
     }
@@ -50,7 +86,10 @@ export function runTradeForSystem(system: System, revolutions: number): System {
 function getShortagesFromStation(station: Station): ItemType[] {
   const allShortages: ItemType[] = [];
   for (const itemType of allItemTypes) {
-    const baseQuantity = Trd.getItemBaseQuantity(station.tradeInventory, itemType);
+    const baseQuantity = Trd.getItemBaseQuantity(
+      station.tradeInventory,
+      itemType
+    );
     const currentQuantity = Trd.getItemCount(station.tradeInventory, itemType);
     if (currentQuantity / baseQuantity <= SHORTAGE_THRESHOLD) {
       allShortages.push(itemType);
@@ -62,8 +101,11 @@ function getShortagesFromStation(station: Station): ItemType[] {
 // Note we clone the system, which makes a new System,
 // but contains the same Stations instances.
 function sortSystemByDistance(system: System, station: Station) {
-  const distFromRequester = (x: Station) => getDistance(station.position, x.position);
-  const systemSortedByDistance = cloneSystem(system).sort((a, b) => distFromRequester(a) - distFromRequester(b));
+  const distFromRequester = (x: Station) =>
+    getDistance(station.position, x.position);
+  const systemSortedByDistance = cloneSystem(system).sort(
+    (a, b) => distFromRequester(a) - distFromRequester(b)
+  );
   return systemSortedByDistance;
 }
 
@@ -74,7 +116,10 @@ function getNearestSupplier(
   itemType: ItemType,
   itemQuantity: number
 ): Station | null {
-  const systemSortedByDistance = sortSystemByDistance(system, requestingStation);
+  const systemSortedByDistance = sortSystemByDistance(
+    system,
+    requestingStation
+  );
   for (const supplier of systemSortedByDistance) {
     // Avoid trading if it puts the supplier into a shortage (this avoids shortage loops)
     if (hasTradeableAmount(supplier, itemType, itemQuantity)) return supplier;
@@ -83,8 +128,14 @@ function getNearestSupplier(
 }
 
 // function: Get random neighbor in range
-function getRandomNeighborInRange(system: System, station: Station, range: number) {
-  const stationsWithinRange = system.filter((stn) => stationDistance(station, stn) <= range);
+function getRandomNeighborInRange(
+  system: System,
+  station: Station,
+  range: number
+) {
+  const stationsWithinRange = system.filter(
+    (stn) => stationDistance(station, stn) <= range
+  );
   const randomPick = randomOf(stationsWithinRange);
   return randomPick;
 }
@@ -105,27 +156,23 @@ function tradeBetween(
   supplier: Station;
 } {
   const supply = Trd.getItemCount(supplier.tradeInventory, itemType);
-  // if (supply < itemQuantity) {
-  //   console.log(`Error! ${supply} < ${itemQuantity}`);
-  // }
-  // if (isNaN(itemQuantity)) {
-  //   console.log(`Error! itemQuantity WAS ALREADY NaNd`);
-  // }
+
   const returnValue = {
     requester: set(requester, {
-      tradeInventory: Trd.addItemCount(requester.tradeInventory, itemType, itemQuantity),
+      tradeInventory: Trd.addItemCount(
+        requester.tradeInventory,
+        itemType,
+        itemQuantity
+      ),
     }),
     supplier: set(supplier, {
-      tradeInventory: Trd.addItemCount(supplier.tradeInventory, itemType, -itemQuantity),
+      tradeInventory: Trd.addItemCount(
+        supplier.tradeInventory,
+        itemType,
+        -itemQuantity
+      ),
     }),
   };
-  // const newSupply = Trd.getItemCount(
-  //   returnValue.supplier.tradeInventory,
-  //   itemType
-  // );
-  // if (isNaN(newSupply)) {
-  //   console.log(`Error! ${newSupply} NaNd`);
-  // }
   return returnValue;
 }
 
@@ -135,31 +182,62 @@ function stationDistance(station1: Station, station2: Station): number {
 }
 
 /** Returns true if the station can lose specified quantity without causing a shortage. */
-function hasTradeableAmount(station: Station, itemType: ItemType, itemQuantity: number): boolean {
+function hasTradeableAmount(
+  station: Station,
+  itemType: ItemType,
+  itemQuantity: number
+): boolean {
   const stationQuantity = Trd.getItemCount(station.tradeInventory, itemType);
-  const stationBaseQuantity = Trd.getItemBaseQuantity(station.tradeInventory, itemType);
-  const minimumQuantityToTrade = itemQuantity + stationBaseQuantity / SHORTAGE_THRESHOLD;
+  const stationBaseQuantity = Trd.getItemBaseQuantity(
+    station.tradeInventory,
+    itemType
+  );
+  const minimumQuantityToTrade =
+    itemQuantity + stationBaseQuantity / SHORTAGE_THRESHOLD;
   return stationQuantity > minimumQuantityToTrade;
 }
 
-function randomTradeBetween(requester: Station, supplier: Station): { requester: Station; supplier: Station } | null {
+function randomTradeBetween(
+  requester: Station,
+  supplier: Station
+): { requester: Station; supplier: Station } | null {
   // Find all possible items supplier has a quantity of
   const requesterItems = getItemTypesForStation(requester);
   let possibleItems: ItemType[] = [];
   for (const itemType of allItemTypes) {
-    const supplierQuantity = Trd.getItemCount(supplier.tradeInventory, itemType);
-    if (supplierQuantity > 0 && requesterItems.some((item) => itemType === item)) possibleItems.push(itemType);
+    const supplierQuantity = Trd.getItemCount(
+      supplier.tradeInventory,
+      itemType
+    );
+    if (
+      supplierQuantity > 0 &&
+      requesterItems.some((item) => itemType === item)
+    )
+      possibleItems.push(itemType);
   }
 
   // Pick a random amount that won't cause a shortage
   for (const itemType of shuffle(possibleItems)) {
-    const supplierQuantity = Trd.getItemCount(supplier.tradeInventory, itemType);
-    const supplierBaseQuantity = Trd.getItemBaseQuantity(supplier.tradeInventory, itemType);
+    const supplierQuantity = Trd.getItemCount(
+      supplier.tradeInventory,
+      itemType
+    );
+    const supplierBaseQuantity = Trd.getItemBaseQuantity(
+      supplier.tradeInventory,
+      itemType
+    );
     if (supplierBaseQuantity === undefined) continue;
-    const tradeableQuantity = supplierQuantity - supplierBaseQuantity * SHORTAGE_THRESHOLD;
+    const tradeableQuantity =
+      supplierQuantity - supplierBaseQuantity * SHORTAGE_THRESHOLD;
     if (tradeableQuantity <= 0) continue;
-    const maximum = Math.min(tradeableQuantity, supplierBaseQuantity * MAX_TRADE);
-    const minimum = Math.min(tradeableQuantity, supplierBaseQuantity * MIN_TRADE);
+    const maximum = Math.min(
+      tradeableQuantity,
+      supplierBaseQuantity * MAX_TRADE
+    );
+    const minimum = Math.min(
+      tradeableQuantity,
+      supplierBaseQuantity * MIN_TRADE
+    );
     const randomTradeQuantity = randomInt(minimum, maximum);
     return tradeBetween(requester, supplier, itemType, randomTradeQuantity);
   }
