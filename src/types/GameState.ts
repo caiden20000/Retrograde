1354ab5e80b1cc802ae23709c725b14e804dc522
c2k <121005054+caiden20000@users.noreply.github.com>
2025-07-22 04:36:21 +0000

    Include encounter in game state

--- src/types/GameState.ts
+++ src/types/GameState.ts
@@ -1,4 +1,6 @@
+import { Battle } from "./Battle";
 import { Character } from "./Character";
+import { EncounterNode } from "./EncounterNode";
 import { ScreenType } from "./ScreenType";
 import { SpaceDate } from "./SpaceDate";
 import { System } from "./System";
@@ -8,6 +10,14 @@ export type GameState = {
   player: Character;
   system: System;
   date: SpaceDate;
-  travel: Travel | null;
   currentScreen: ScreenType;
-};
+} & (
+  | {
+      travel: null;
+      encounter: null;
+    }
+  | {
+      travel: Travel;
+      encounter: EncounterNode | null;
+    }
+);
