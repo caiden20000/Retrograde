a279cd8ec02e931622f9b740d103df8eadb4ceda
c2k <121005054+caiden20000@users.noreply.github.com>
2025-07-24 07:53:23 +0000

    micro version

--- src/components/watermark.tsx
+++ src/components/watermark.tsx
@@ -1,9 +1,14 @@
 export function Watermark() {
   return (
     <div className="watermark">
-      <img src="./Retrograde_Logo.png" alt="Retrograde Logo" className="logo" width={32} />
+      <img
+        src="./Retrograde_Logo.png"
+        alt="Retrograde Logo"
+        className="logo"
+        width={32}
+      />
       <div className="title">Retrograde</div>
-      <div className="version">v0.0.2</div>
+      <div className="version">v0.0.3</div>
     </div>
   );
 }
